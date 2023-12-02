74ed8006f202570f8490cb0a9c602691919e4972
Matheus Machado <agromsmachado@gmail.com>
2023-12-02 14:58:47 -0300

    resposive

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import { Header } from './Components/Header'
 import { Task } from './Components/Task'
 import { TaskCounter } from './Components/TaskCouter'
 import './global.scss'
+import styles from './App.module.scss';
 
 function App() {
 
   return (
     <>
       <Header />
-      <AddTask />
-      <TaskCounter />
-      <Task />
-      <Task />
-      <Task />
+      <section className={styles.mainWrapper}>
+        <AddTask />
+        <TaskCounter />
+        <Task />
+        <Task />
+        <Task />
+      </section>
     </>
   )
 }
